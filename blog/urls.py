b15236b5f556ad0dcf6895064885f5082630237a
Garvi <garvillo@gmail.com>
2017-12-08 03:49:22 +0100

    ?: (urls.W005) URL namespace 'admin' isn't unique.

--- blog/urls.py
+++ blog/urls.py
@@ -6,9 +6,10 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
 	url(r'^$', views.post_list, name='home'),
 
+    url(r'^home/$', auth_views.login, {'template_name': 'registration/home.html'}, name='home'),
   # url(r'^login/$', auth_views.login, name='login'),
     url(r'^login/$', auth_views.login, {'template_name': 'registration/login.html'}, name='login'),
   # url(r'^logout/$', auth_views.logout, name='logout'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'registration/logged_out.html'}, name='logout'),
-    #url(r'^admin/', admin.site.urls),
+  #	url(r'^admin/', admin.site.urls),
 ]
